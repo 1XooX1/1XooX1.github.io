e09f35bea3faa0ab9795c588a9f5d281e7b8338e
1XooX1 <146197650+1XooX1@users.noreply.github.com>
2023-12-16 11:38:06 +0500

    script.js

--- script.js
+++ script.js
@@ -4,23 +4,16 @@ const gifContainer = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
-// Replace 'AUDIO_URL' with the actual URL of your audio file
-const audioUrl = 'https://audio.jukehost.co.uk/Oi1X7JzQQhmHPoJMxaQpUPfEaaJnlMgy';
-
-const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-let audioBuffer;
-let audioSource;
-
 // Giphy direct GIF links
 const giphyLinks = [
   "https://media.giphy.com/media/3tE1YGjnDP6dPnL8qw/giphy.gif", // Default gif link
-  "https://media.giphy.com/media/pJzetkW7ryIfDjK8gc/giphy.gif", // Replace with your second Giphy link
+  "", // Replace with your video link
   "https://media.giphy.com/media/JnsTyQxv02GIbiJuNS/giphy.gif" // Replace with your "No" button Giphy link
 ];
 
 // Corresponding text for each gif
 const gifTexts = [
-  "✨Will you be my Jasmine?😋", // Default gif text
+   "✨Will you be my Jasmine?😋", // Default gif text
   "Congratulations 🎉 you're officially mine and I'm yours✨☝️🤓", // Second gif text
   "Hmm, no?🥺" // "No" button gif text
 ];
@@ -45,19 +38,18 @@ yesBtn.addEventListener("click", () => {
   const secondGifIndex = 1;
   question.innerHTML = gifTexts[secondGifIndex];
 
-  // Change to the second gif after clicking "Yes"
+  // Change to the video after clicking "Yes"
   gifContainer.src = giphyLinks[secondGifIndex];
-
-  // Stop audio if it's already playing
-  stopAudio();
-
-  // Play audio when "Yes" is clicked
-  playAudio();
 });
 
 noBtn.addEventListener("click", () => {
-  // Stop audio if it's already playing
-  stopAudio();
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  // Change position of "No" button
+  noBtn.style.left = Math.floor(Math.random() * maxX) + "px";
+  noBtn.style.top = Math.floor(Math.random() * maxY) + "px";
 
   // Play a gif when "No" is clicked
   const noGifIndex = 2; // Replace with the index of the "No" button gif
@@ -67,7 +59,4 @@ noBtn.addEventListener("click", () => {
   const noButtonTextIndex = 2; // Replace with the index of the "No" button text
   question.innerHTML = gifTexts[noButtonTextIndex];
 });
-
-
-  }
-}
+      
