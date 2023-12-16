122ba702a87273a3bbdaac4f9a91a5ab25fc65bd
1XooX1 <146197650+1XooX1@users.noreply.github.com>
2023-12-16 11:40:31 +0500

    script.js

--- script.js
+++ script.js
@@ -4,20 +4,25 @@ const gifContainer = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
+// Replace 'AUDIO_URL' with the actual URL of your audio file
+const audioUrl = '';
+
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+let audioBuffer;
+
 // Giphy direct GIF links
 const giphyLinks = [
   "https://media.giphy.com/media/3tE1YGjnDP6dPnL8qw/giphy.gif", // Default gif link
-  "", // Replace with your video link
+  "https://media.giphy.com/media/pJzetkW7ryIfDjK8gc/giphy.gif", // Replace with your second Giphy link
   "https://media.giphy.com/media/JnsTyQxv02GIbiJuNS/giphy.gif" // Replace with your "No" button Giphy link
 ];
 
 // Corresponding text for each gif
 const gifTexts = [
-   "✨Will you be my Jasmine?😋", // Default gif text
+ "✨Will you be my Jasmine?😋", // Default gif text
   "Congratulations 🎉 you're officially mine and I'm yours✨☝️🤓", // Second gif text
   "Hmm, no?🥺" // "No" button gif text
 ];
-
 // Preload Giphy GIFs
 const preloadGifs = () => {
   giphyLinks.forEach((giphyLink) => {
@@ -33,13 +38,16 @@ const defaultGifIndex = 0;
 gifContainer.src = giphyLinks[defaultGifIndex];
 question.innerHTML = gifTexts[defaultGifIndex];
 
-yesBtn.addEventListener("click", () => {
+yesBtn.addEventListener("click", async () => {
   // Change text after clicking "Yes"
   const secondGifIndex = 1;
   question.innerHTML = gifTexts[secondGifIndex];
 
-  // Change to the video after clicking "Yes"
+  // Change to the second gif after clicking "Yes"
   gifContainer.src = giphyLinks[secondGifIndex];
+
+  // Play audio when "Yes" is clicked
+  await playAudio();
 });
 
 noBtn.addEventListener("click", () => {
@@ -59,4 +67,19 @@ noBtn.addEventListener("click", () => {
   const noButtonTextIndex = 2; // Replace with the index of the "No" button text
   question.innerHTML = gifTexts[noButtonTextIndex];
 });
-      
+
+async function playAudio() {
+  try {
+    const response = await fetch(audioUrl);
+    const arrayBuffer = await response.arrayBuffer();
+    audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+
+    const source = audioContext.createBufferSource();
+    source.buffer = audioBuffer;
+    source.connect(audioContext.destination);
+
+    source.start();
+  } catch (error) {
+    console.error("Error loading or playing audio:", error);
+  }
+  }
